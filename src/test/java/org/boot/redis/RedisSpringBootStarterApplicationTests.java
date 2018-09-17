add745067195fe2620d42a8b52062273e34d78ac
heart <15951934264@qq.com>
2018-09-18 07:01:47 +0800

    单元测试示例修改

--- src/test/java/org/boot/redis/RedisSpringBootStarterApplicationTests.java
+++ src/test/java/org/boot/redis/RedisSpringBootStarterApplicationTests.java
@@ -1,5 +1,6 @@
 package org.boot.redis;
 
+import org.boot.redis.core.SpringJedisClusterService;
 import org.boot.redis.core.SpringJedisStandAloneService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,16 +16,23 @@ public class RedisSpringBootStarterApplicationTests {
      * Redis单机服务
      */
     @Autowired
-    SpringJedisStandAloneService service;
+    SpringJedisStandAloneService standAloneService;
+
+    /**
+     * Redis集群服务 正在实现
+     */
+    @Autowired
+    SpringJedisClusterService clusterService;
 
     @Test
     public void contextLoads() {
+        //单机版本操作
         //第一个参数 数据库索引
         //第二个参数 key
         //第三个参数 value
-        System.out.println(service.set(1, "test", "123"));
-        System.out.println(service.exists(1, "test"));
-        System.out.println(service.incr(2, "test"));
+        System.out.println(standAloneService.set(1, "test", "123"));
+        System.out.println(standAloneService.exists(1, "test"));
+        System.out.println(standAloneService.incr(2, "test"));
     }
 
 }
